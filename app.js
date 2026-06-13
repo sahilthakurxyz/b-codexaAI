b068cc289337a4b9c061f0b52ac22e8ec77beda0
sahilthakurxyz <sahilthakurxyz@gmail.com>
2026-06-14 02:44:27 +0530

    change cross origin

--- app.js
+++ app.js
@@ -8,15 +8,17 @@ import cors from "cors";
 export const app = express();
 app.use(express.json());
 app.use(cookieParser());
+
 // app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-    methods: ["GET,HEAD,POST,PATCH,DELETE"],
+    methods: ["GET", "HEAD", "POST", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
+
 // app.options("*", cors({ origin: process.env.FRONTEND_URL }));
 app.use("/api/ai/v1/", authRoute);
 app.use("/api/ai/v1/chat/", chatRoute);
